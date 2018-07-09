37e86ebb4744e65c66a7e8242a702bb2a7f17c10
zjuzzy <33485704+zjuzzy@users.noreply.github.com>
2018-07-09 15:06:52 +0800

    Update delete_declaration.py

--- prcv2018/data_tools/delete_declaration.py
+++ prcv2018/data_tools/delete_declaration.py
@@ -1,3 +1,9 @@
+
+import hashlib
+import io
+import os
+
+import tensorflow as tf
 from xml.etree.ElementTree import ElementTree, Element
 
 
@@ -24,8 +30,42 @@ def if_match(node, kv_map):
             return False
     return True
 
-if __name__ == "__main__":
-    # 1. 读取xml文件
-    tree = read_xml("/home/zju/models/research/object_detection/Det_datasets/train_data/0_1/xml/1_8.xml")
-    # 2. 输出到结果文件
-    write_xml(tree, "/home/zju/models/research/object_detection/Det_datasets/train_data/0_1/xml/1_9.xml")
+
+
+def read_examples_list(path):
+    """Read list of training or validation examples.
+
+  The file is assumed to contain a single example per line where the first
+  token in the line is an identifier that allows us to find the image and
+  annotation xml for that example.
+
+  For example, the line:
+  xyz 3
+  would allow us to find files xyz.jpg and xyz.xml (the 3 would be ignored).
+
+  Args:
+    path: absolute path to examples list file.
+
+  Returns:
+    list of example identifiers (strings).
+    """
+    with tf.gfile.GFile(path) as fid:
+        lines = fid.readlines()
+        image_info = [line.strip().split(' ')[0] for line in lines]
+        annotation_info = [line.strip().split(' ')[1] for line in lines]
+    return (annotation_info)
+
+
+
+def main(_):
+    txt_path = os.path.join('Det_datasets', 'train_list' + '.txt')
+    examples_list = read_examples_list(txt_path)
+    for idx, example in enumerate(examples_list):
+        label_path = os.path.join('Det_datasets', example)
+        # 1. 读取xml文件
+        tree = read_xml(label_path)
+        # 2. 输出到结果文件
+        write_xml(tree,label_path)
+
+if __name__ == '__main__':
+  tf.app.run()
